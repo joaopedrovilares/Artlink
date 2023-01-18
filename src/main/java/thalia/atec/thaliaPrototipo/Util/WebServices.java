e90ec2915357eda3ca82780f71cb71cd7ddbc637
João Pedro Vilares <44915510+joaopedrovilares@users.noreply.github.com>
2023-01-18 07:10:37 +0000

    Various Fixes

--- src/main/java/thalia/atec/thaliaPrototipo/Util/WebServices.java
+++ src/main/java/thalia/atec/thaliaPrototipo/Util/WebServices.java
@@ -2,10 +2,7 @@ package thalia.atec.thaliaPrototipo.Util;
 
 public class WebServices {
 	
-
-//	public static final String SERVER = "35.228.150.66";
-	
-	public static final String SERVER = "http://artlink.pt";
+public static final String SERVER = "http://artlink.pt";
 	
 
 }
